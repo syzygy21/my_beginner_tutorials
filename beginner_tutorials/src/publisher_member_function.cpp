2515312123f95f41906f2b1c2940b079f00fd5c0
Navdeep Singh <singh.nav2100@gmail.com>
2024-11-06 18:04:51 -0500

    Modified the string published by the publisher as instructed in the problem.  The modified string is 'I am Navdeep'

--- beginner_tutorials/src/publisher_member_function.cpp
+++ beginner_tutorials/src/publisher_member_function.cpp
@@ -62,7 +62,7 @@ class MinimalPublisher : public rclcpp::Node {
    */
   void timer_callback() {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, world! " + std::to_string(count_++);
+    message.data = "I am Navdeep. " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
